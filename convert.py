f228466eeb7ab96802e024e83a65a2b6e9be5fb5
Keith Sanders <sanders.kl19@yahoo.com>
2019-06-08 14:45:22 -0500

    Delete convert.py

--- convert.py
+++
@@ -1,8 +0,0 @@
-temperatures = [10, -20, 100]
-
-def cel_to_fahr(celsius):
-    fahrenheit = celsius * 9/5 + 32
-    return fahrenheit
-
-for temperature in temperatures:
-    print(cel_to_fahr(temperature))
